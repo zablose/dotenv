e74603ec318296802c83c255015812d6bfa6b0a5
Sergejs Zablockis <zablose@gmail.com>
2024-07-13 09:49:50 +0100

    Make sure, that second parameter for 'str_replace' is string.

--- src/Variable.php
+++ src/Variable.php
@@ -60,7 +60,7 @@ class Variable
 
         while ($var_start !== false && $var_end !== false && $var_end > $var_start) {
             $var_name = substr($value, $var_start + 2, $var_end - $var_start - 2);
-            $var_value = $vars[$var_name] ?? 'undefined';
+            $var_value = (string) $vars[$var_name] ?? 'undefined';
             $value = str_replace('${'.$var_name.'}', $var_value, $value);
             $var_start = strpos($value, '${');
             $var_end = strpos($value, '}');
